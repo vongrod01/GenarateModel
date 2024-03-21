a03253d27ea8c042539d01559eb522445c472d5b
adisorn@redtech.co.th <aum>
2024-03-21 09:21:34 +0700

    add bg-image

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ const session = require('express-session')
 
 const app = express()
 app.use(cors())
+// let genuuid=require('uuid');
 const fs = require("fs")
 const connectDB_router = require('./routers/connectDB_router')
 const exportFile_router = require('./routers/exportFile_router')
 const view_router = require('./routers/view_router')
 // const userRouter = require('./routers/userAPI')
-
+function genuuid(){
+    return require('uuid')
+}
 // app.use(cookieParser());
 // app.use(csrfMiddleware);
 app.use(express.json());
@@ -20,15 +23,20 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs') // ทำให้ html เป็น dinamic โดยใช้ ejs
 app.use(express.urlencoded({ extended: false }))
 
-app.use(session({
-    name: 'SessionCookie',
-    secret: 'Shsh!Secret!',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false, expires: 60000 }
-}))
+app.use(session(
+    {
+        name: 'SessionCookie',
+        genid: function (req) {
+            // console.log('session id created');
+            return genuuid();
+        },
+        secret: 'Shsh!Secret!',
+        resave: false,
+        saveUninitialized: false,
+        cookie: { secure: false, expires: 60000 }
+    }));
 // user router
-app.use(view_router,connectDB_router,exportFile_router)
+app.use(view_router, connectDB_router, exportFile_router)
 // app.use(user)
 app.use(express.static(path.join(__dirname, 'node_modules')))
 app.use(express.static(path.join(__dirname, 'public')))
